71e31caba716b56e51069c34ed3a271ed5a1dcdd
Asthaviradiya <asthaviradiya114@gmail.com>
2024-07-19 19:17:49 +0530

    exploreMenu and header component is added

---
+++ Tomato-App/src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='header-content'>
+            <h2>Order Your Favorite Food Here</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus neque enim inventore impedit modi at quidem, perferendis, aspernatur, cumque reprehenderit accusamus provident reiciendis in ex aliquid saepe magni nostrum obcaecati?</p>
+            <button>view menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
